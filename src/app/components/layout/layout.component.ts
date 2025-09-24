b1e8f0c0844d505cdeca14e3b406239c6d02f0ab
Bharti Dhote <2023kucp1049@iiitkota.ac.in>
2025-09-24 22:06:40 +0530

    first commit

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { QuickNavComponent } from './quick-nav.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, QuickNavComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {}
